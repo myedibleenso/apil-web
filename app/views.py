96259a6b8339f9427ba6931fae5728b553b729c7
Gus Hahn-Powell <gushahnpowell@gmail.com>
2015-01-13 23:09:33 -0700

    Added some simple error handling.

--- app/views.py
+++ app/views.py
@@ -5,6 +5,19 @@ from app import app
 def index():
     return "Hello, World!"
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'This page does not exist', 404
+
+#@app.errorhandler(DatabaseError)
+#def special_exception_handler(error):
+#    return 'Database connection failed', 500
+
 @app.route('/draw')
 def poop():
     return app.send_static_file('draw.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
--- run.py
+++ run.py
@@ -1,3 +1,7 @@
 #!apil_env/bin/python
 from app import app
-app.run(debug=True)
+
+if __name__ == '__main__':
+    app.debug = True
+    #app.run(host="0.0.0.0", port=8080)
+    app.run()
