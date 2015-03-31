d8b0047a7add60b0c771ad7a434a8c5860a99d4f
Gus Hahn-Powell <gushahnpowell@gmail.com>
2015-03-31 12:30:46 -0700

    Should I add an option to trim points beyond RoI?

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,12 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField, HiddenField
+from wtforms import StringField, SubmitField, HiddenField, SelectField
 from wtforms.validators import Required
 
 class TraceForm(Form):
     name = StringField('Tracer ID', id='tracer', validators=[Required()])
     subject = StringField('Subject', id='subject')
     project_id = StringField('Project ID', id='project', validators=[Required()])
+    #trim_points = SelectField(u'Remove points outside RoI?', choices=[(False, 'No'), (True, 'Yes')], validators=[Required()])
     data = HiddenField(id='trace-data')
     roi = HiddenField(id='roi-data')
     submit = SubmitField('Get traces', id='dump-traces')
